ffe7f4a5b07f3db111df20ca78a3d3c48c8bc05b
yas872 <alinefelimafra@gmail.com>
2026-02-11 10:19:49 -0300

    Add files via upload

---
+++ Projeto-integrador/PI/src/PI/src/teste2.java
@@ -0,0 +1,76 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+ 
+
+public class teste2 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					teste2 frame = new teste2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public teste2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1112, 581);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(52, 176, 243));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(126, 201, 255));
+		contentPane.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new MigLayout("", "[grow][left][][][]", "[][][]"));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(new Color(240, 240, 240));
+		lblNewLabel.setIcon(new ImageIcon(teste2.class.getResource("/img/alarm.png")));
+		panel.add(lblNewLabel, "flowx,cell 0 1");
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(teste2.class.getResource("/routinehacker/ROUTINE HACKER.png")));
+		panel.add(label, "cell 0 1");
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel.add(lblNewLabel_2, "cell 2 1");
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		panel.add(lblNewLabel_3, "cell 3 1");
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+
+	}
+
+}

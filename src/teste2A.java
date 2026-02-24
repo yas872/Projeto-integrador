39efa346ec20e22b77572b38e60d8291ddebd840
Your Name <you@example.com>
2026-02-24 11:45:56 -0300

    commit02

--- src/teste2A.java
+++ src/teste2A.java
@@ -27,7 +27,7 @@ public class teste2A extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					teste2 frame = new teste2();
+					teste2A frame = new teste2A();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -55,21 +55,40 @@ public class teste2A extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setBackground(new Color(240, 240, 240));
-		lblNewLabel.setIcon(new ImageIcon(teste2.class.getResource("/img/alarm.png")));
+		lblNewLabel.setIcon(new ImageIcon(teste2A.class.getResource("/img/alarm.png")));
 		panel.add(lblNewLabel, "flowx,cell 0 1");
 		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(teste2.class.getResource("/routinehacker/ROUTINE HACKER.png")));
+		label.setIcon(new ImageIcon(teste2A.class.getResource("/routinehacker/ROUTINE HACKER.png")));
 		panel.add(label, "cell 0 1");
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		panel.add(lblNewLabel_4, "cell 1 1");
+		
+		JLabel lblNewLabel_2 = new JLabel("\r\n");
+		lblNewLabel_2.setIcon(new ImageIcon(teste2A.class.getResource("/img/contato.png")));
 		panel.add(lblNewLabel_2, "cell 2 1");
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon(teste2A.class.getResource("/img/entrar.png")));
 		panel.add(lblNewLabel_3, "cell 3 1");
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(243, 255, 255));
 		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new MigLayout("", "[grow][][grow]", "[grow]"));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(243, 255, 255));
+		panel_1.add(panel_2, "cell 0 0,grow");
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(teste2A.class.getResource("/img/Full-width.png")));
+		panel_1.add(lblNewLabel_1, "cell 1 0");
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(243, 255, 255));
+		panel_1.add(panel_3, "cell 2 0,grow");
 
 	}
 
